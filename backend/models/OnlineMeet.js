1ca821fd5f4ecdcf1063a75cea5d59edcb54edab
Trun6 <trungdtgch210921@fpt.edu.vn>
2025-03-09 23:07:30 +0700

    123

--- backend/models/OnlineMeet.js
+++ backend/models/OnlineMeet.js
@@ -4,7 +4,7 @@ const OnlineMeetSchema = new mongoose.Schema({
     title: { type: String, required: true },
     date: { type: Date, required: true },
     participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    meetLink: { type: String, required: true }, // Link Google Meet
+    meetLink: { type: String, required: true }, 
 }, { timestamps: true });
 
 module.exports = mongoose.model("OnlineMeet", OnlineMeetSchema);
